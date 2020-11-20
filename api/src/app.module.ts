641e3dd6f333452c362676761b8dd808943eee78
Chinedu Ifediorah <cifediorah3@gmail.com>
2020-11-20 11:33:53 +0100

    chore(database setup): intialize database connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './config/typeorm.config';
 import { UserRoleModule } from './user-role/user-role.module';
 
 @Module({
-  imports: [UserRoleModule],
+  imports: [UserRoleModule, TypeOrmModule.forRoot(typeormConfig)],
 })
 export class AppModule {}
